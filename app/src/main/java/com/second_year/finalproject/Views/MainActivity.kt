c2279fb68905b78113f35ff7e590274ee66d1f93
naxareth <acephilipdenulan12@gmail.com>
2025-02-06 14:04:57 +0800

    another setup

--- app/src/main/java/com/second_year/finalproject/Views/MainActivity.kt
+++ app/src/main/java/com/second_year/finalproject/Views/MainActivity.kt
@@ -1,10 +1,11 @@
-package com.second_year.finalproject
+package com.second_year.finalproject.Views
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.second_year.finalproject.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
