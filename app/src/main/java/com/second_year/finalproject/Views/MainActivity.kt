e774b7d3350c2707b2dab480f66874e4614e752c
Feniel Barte <feambarte.up@phinmaaed.com>
2025-02-17 16:00:01 +0800

    Layout

--- app/src/main/java/com/second_year/finalproject/Views/MainActivity.kt
+++ app/src/main/java/com/second_year/finalproject/Views/MainActivity.kt
@@ -1,21 +1,105 @@
 package com.second_year.finalproject.Views
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.second_year.finalproject.Api.Interfaces.ForgotPasswordActivity
+import com.second_year.finalproject.Api.RetrofitClient
 import com.second_year.finalproject.R
+import okhttp3.ResponseBody
+import org.json.JSONException
+import com.second_year.finalproject.Api.Interfaces.LoginActivity
+import org.json.JSONObject
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), android.view.View.OnClickListener {
+    private lateinit var etUsername: EditText
+    private lateinit var etPassword: EditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        etUsername = findViewById(R.id.etUsername)
+        etPassword = findViewById(R.id.etPassword)
+
+        val buttonSignUp: Button = findViewById(R.id.buttonSignUp)
+        val textViewLogin: TextView = findViewById(R.id.textViewLogin)
+
+        buttonSignUp.setOnClickListener(this)
+        textViewLogin.setOnClickListener(this)
+
+        findViewById<TextView>(R.id.textViewForgotPassword).setOnClickListener {
+            startActivity(Intent(this, ForgotPasswordActivity::class.java))
         }
+    }
+
+    override fun onClick(view: android.view.View) {
+        when (view.id) {
+            R.id.buttonSignUp -> userSignup()
+            R.id.textViewLogin -> startActivity(Intent(this, LoginActivity::class.java))
+        }
+    }
+
+    private fun userSignup() {
+        val username = etUsername.text.toString().trim()
+        val password = etPassword.text.toString().trim()
+
+        if (username.isEmpty()) {
+            etUsername.error = "Username is Required"
+            etUsername.requestFocus()
+            return
+        }
+
+        if (password.isEmpty()) {
+            etPassword.error = "Password is Required"
+            etPassword.requestFocus()
+            return
+        }
+
+        if (password.length < 6) {
+            etPassword.error = "Password should be at least 6 characters long"
+            etPassword.requestFocus()
+            return
+        }
+
+        val call: Call<ResponseBody> = RetrofitClient.api.createUser(username, password)
+
+
+        call.enqueue(object : Callback<ResponseBody> {
+            override fun onResponse(call: Call<ResponseBody>, response: Response<ResponseBody>) {
+                var s: String? = response.body()?.string()
+
+                if (s != null) {
+                    try {
+                        val jsonObject = JSONObject(s)
+                        Toast.makeText(
+                            this@MainActivity,
+                            jsonObject.getString("message"),
+                            Toast.LENGTH_LONG
+                        ).show()
+
+                        if (jsonObject.getString("message") == "User registered successfully") {
+                            val intent = Intent(this@MainActivity, LoginActivity::class.java)
+                            startActivity(intent)
+                            finish()
+                        }
+
+                    } catch (e: JSONException) {
+                        e.printStackTrace()
+                    }
+                }
+            }
+
+            override fun onFailure(call: Call<ResponseBody>, t: Throwable) {
+                Toast.makeText(this@MainActivity, t.message, Toast.LENGTH_LONG).show()
+            }
+        })
     }
 }
